3dd1109cafd626c728e03391a3ee791b33294bf1
Augusto Balsanelli <augustebalsanel@proton.me>
2023-06-30 11:21:21 -0300

    Updated models

--- library/admin.py
+++ library/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from library.models import Author, Category, Publisher
+from library.models import Author, Book, Category, Publisher
 
 admin.site.register(Author)
+admin.site.register(Book)
 admin.site.register(Category)
 admin.site.register(Publisher)
--- library/models.py
+++ library/models.py
@@ -33,3 +33,14 @@ class Publisher(models.Model):
     class Meta:
         verbose_name = 'Publisher'
         verbose_name_plural = 'Publishers'
+
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    isbn = models.CharField(max_length=32, null=True, blank=True)
+    amount = models.IntegerField(default=0, null=True, blank=True)
+    price = models.DecimalField(max_digits=7, decimal_places=2, null=True, blank=True)
+    category = models.ForeignKey(Category, on_delete=models.PROTECT, related_name='books')
+    publisher = models.ForeignKey(Publisher, on_delete=models.PROTECT, related_name='books')
+
+    def __str__(self):
+        return f"{self.title} {self.amount}"
