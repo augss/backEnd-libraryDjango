be5a0790aeb63bec1d32a43f97046c7a48baede5
Augusto Balsanelli <augustebalsanel@proton.me>
2023-06-30 10:56:46 -0300

    Created publisher model

--- library/models.py
+++ library/models.py
@@ -1,2 +1,16 @@
 from django.db import models
 
+
+class Category(models.Model):
+    description = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.description
+
+
+class Publisher(models.Model):
+    name = models.CharField(max_length=100)
+    website = models.URLField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
