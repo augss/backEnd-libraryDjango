c46c23284433e097ecf186f026422fe00947d92b
Augusto Balsanelli <augustebalsanel@proton.me>
2023-06-30 11:02:10 -0300

    Created author model

--- library/admin.py
+++ library/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from library.models import Category, Publisher
+from library.models import Author, Category, Publisher
 
+admin.site.register(Author)
 admin.site.register(Category)
 admin.site.register(Publisher)
--- library/models.py
+++ library/models.py
@@ -1,11 +1,26 @@
 from django.db import models
 
+class Author(models.Model):
+    name = models.CharField(max_length=255)
+    email = models.EmailField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name = 'Author'
+        verbose_name_plural = 'Authors'
+
 
 class Category(models.Model):
     description = models.CharField(max_length=100)
 
     def __str__(self):
         return self.description
+    
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
 
 
 class Publisher(models.Model):
@@ -14,3 +29,7 @@ class Publisher(models.Model):
 
     def __str__(self):
         return self.name
+
+    class Meta:
+        verbose_name = 'Publisher'
+        verbose_name_plural = 'Publishers'
