46db12dcc29a646de2dada537b6be381059c872e
Augusto Balsanelli <augustebalsanel@proton.me>
2023-06-30 13:07:58 -0300

    Updated book seralizer and viewset

--- library/views.py
+++ library/views.py
@@ -3,15 +3,22 @@ from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet
 
 from library.models import Author, Book, Category, Publisher
-from library.serializers import AuthorSerializer, BookSerializer, CategorySerializer, PublisherSerializer
+from library.serializers import AuthorSerializer, BookSerializer, BookDetailSerializer, BookListSerializer, CategorySerializer, PublisherSerializer
 
 class AuthorViewSet(ModelViewSet):
     serializer_class = AuthorSerializer
     queryset = Author.objects.all()
 
 class BookViewSet(ModelViewSet):
-    serializer_class = BookSerializer
     queryset = Book.objects.all()
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return BookListSerializer
+        elif self.action == "retrieve":
+            return BookDetailSerializer
+        return BookSerializer
+
 
 class CategoryViewSet(ModelViewSet):
     serializer_class = CategorySerializer
