dc28e0decb6accb37e018bf4148793933769cbff
Augusto Balsanelli <augustebalsanel@proton.me>
2023-08-07 16:01:11 -0300

    Added authentication

--- library/views.py
+++ library/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from rest_framework.permissions import IsAuthenticated
 
 from rest_framework.viewsets import ModelViewSet
 
@@ -8,6 +9,8 @@ from library.serializers import AuthorSerializer, BookSerializer, BookDetailSeri
 class AuthorViewSet(ModelViewSet):
     serializer_class = AuthorSerializer
     queryset = Author.objects.all()
+    permission_classes = [IsAuthenticated]
+
 
 class BookViewSet(ModelViewSet):
     queryset = Book.objects.all()
@@ -18,12 +21,18 @@ class BookViewSet(ModelViewSet):
         elif self.action == "retrieve":
             return BookDetailSerializer
         return BookSerializer
+    
+    permission_classes = [IsAuthenticated]
+
 
 
 class CategoryViewSet(ModelViewSet):
     serializer_class = CategorySerializer
     queryset = Category.objects.all()
+    permission_classes = [IsAuthenticated]
 
 class PublisherViewSet(ModelViewSet):
     serializer_class = PublisherSerializer
     queryset = Publisher.objects.all()
+    permission_classes = [IsAuthenticated]
+
