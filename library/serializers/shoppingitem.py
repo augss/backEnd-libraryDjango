ce3f40606f3c540d993632325feb4d6bfd702865
Augusto Balsanelli <github.891d8@passfwd.com>
2023-09-04 16:34:34 -0300

    Added pricing total value

--- library/serializers/shoppingitem.py
+++ library/serializers/shoppingitem.py
@@ -1,9 +1,14 @@
-from rest_framework.serializers import ModelSerializer, CharField
+from rest_framework.serializers import ModelSerializer, CharField, SerializerMethodField
 
 from library.models import Shopping, ShoppingItem
 
 class ShoppingItemSerializer(ModelSerializer):
     class Meta:
         model = ShoppingItem
-        fields = ["book", "amount"]
+        fields = ["book", "amount", "total"]
         depth = 2
+
+    total = SerializerMethodField()
+
+    def get_total(self, instance):
+        return instance.amount * instance.book.price
