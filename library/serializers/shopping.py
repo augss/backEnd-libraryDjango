04f403780f0cfd4c3320237bbc2ed5d827fc8730
Augusto Balsanelli <github.891d8@passfwd.com>
2023-09-04 16:39:25 -0300

    Added total value into shopping serializer

--- library/serializers/shopping.py
+++ library/serializers/shopping.py
@@ -1,15 +1,16 @@
-from rest_framework.serializers import ModelSerializer, CharField
-
+from rest_framework.serializers import ModelSerializer, CharField, SerializerMethodField
 from library.serializers import ShoppingItemSerializer
-
 from library.models import Shopping
 
 class ShoppingSerializer(ModelSerializer):
-    class Meta:
-        model = Shopping
-        fields = "__all__"
-
     user = CharField(source="user.email", read_only=True)
     status = CharField(source="get_status_display", read_only=True)
     items = ShoppingItemSerializer(many=True, read_only=True)
+    total = SerializerMethodField()
 
+    class Meta:
+        model = Shopping
+        fields = ("id", "user", "status", "total", "items")
+
+    def get_total(self, obj):
+        return sum(item.book.price * item.amount for item in obj.items.all())
