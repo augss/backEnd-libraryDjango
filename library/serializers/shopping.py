3f03b1de1f018881d03a56d691ef2f0b477ef44a
Augusto Balsanelli <github.891d8@passfwd.com>
2023-09-04 16:23:50 -0300

    Updated shopping status field

--- library/serializers/shopping.py
+++ library/serializers/shopping.py
@@ -6,5 +6,7 @@ class ShoppingSerializer(ModelSerializer):
     class Meta:
         model = Shopping
         fields = "__all__"
-        
+
     user = CharField(source="user.email", read_only=True)
+    status = CharField(source="get_status_display", read_only=True)
+
