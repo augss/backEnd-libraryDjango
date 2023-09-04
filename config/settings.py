95255bc2516a93ff20dbe7c92c54ebdca200ad8e
Augusto Balsanelli <github.891d8@passfwd.com>
2023-09-04 15:49:05 -0300

    Fixed JTW Authetication error

--- config/settings.py
+++ config/settings.py
@@ -136,7 +136,10 @@ REST_FRAMEWORK = {
         "rest_framework.permissions.DjangoModelPermissions",
         "rest_framework.permissions.IsAuthenticated",
         "rest_framework_simplejwt.authentication.JWTAuthentication",
-    ]
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
 }
 
 AUTH_USER_MODEL = "user.User"
